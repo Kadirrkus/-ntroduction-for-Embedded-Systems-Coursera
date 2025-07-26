1e599caa87da9d04c79d2a19922359af80a333d0
Abdulkadir <kadirr.kus58@gmail.com>
2025-07-26 16:45:19 +0300

    Descriptions and Variables

---
+++ stats.c
@@ -0,0 +1,57 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file <stats.c> 
+ * @brief < The implementation file of the c1m1 assignment >
+ *
+ * <A This file includes all the required functions implementations in the c1m1 assignment>
+ *
+ * @author <Abdulkadir Kus>
+ * @date <20.07.2025 >
+ *
+ */
+
+
+
+#include <stdio.h>
+#include "stats.h"
+
+/* Size of the Data Set */
+#define SIZE (40)
+
+int main(void) {
+
+  unsigned char test[SIZE] = { 34, 201, 190, 154,   8, 194,   2,   6,
+                              114, 88,   45,  76, 123,  87,  25,  23,
+                              200, 122, 150, 90,   92,  87, 177, 244,
+                              201,   6,  12,  60,   8,   2,   5,  67,
+                                7,  87, 250, 230,  99,   3, 100,  90};
+                                
+                                
+      /* Other Variable Declarations Go Here */
+                            
+   unsigned char min=0;
+   unsigned char max=0;
+  int mean=0;
+   unsigned  median=0;
+   
+    /* Statistics and Printing Functions Go Here */
+     print_array(test,SIZE)  ;
+     
+   
+    
+    min=find_min(test,SIZE);
+    max=find_max(test,SIZE);
+    mean=find_mean(test,SIZE);
+    median=find_median(test,SIZE);
+    print_statistics(min,max,mean,median);
+    sort_array(test,SIZE);  
+}
---
+++ stats.h
@@ -0,0 +1,21 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file stats.h 
+ * @brief Definitions and macros required for the c1m1 assignment
+ 
+ * @author Abdulkadir Kus
+ * @date 22/07/2025
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
